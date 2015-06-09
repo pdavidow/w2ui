551b660f28b70bfd090cf722141b7e6f0d3fbb7b
Paul Davidowitz <pdavidow@fastmail.fm>
2015-06-09 15:01:17 -0400

    removed redundant schema checking in methods

--- model/lib/methods.es6.js
+++ model/lib/methods.es6.js
@@ -1,33 +1,24 @@
-// http://stackoverflow.com/questions/10984030/get-meteor-collection-by-name
-
 Meteor.methods({
     insert: function(object, collectionName) {
         var collection;
-        var schema;
 
         if (! W2UI_METEOR.PermissionsHelper.isAllowMethodInsert()) return;
 
-        collection = Mongo.Collection.get(collectionName);
-        schema = collection.simpleSchema();
-        check(object, schema);
+        check(collectionName, String);
 
+        collection = Mongo.Collection.get(collectionName);
         return collection.insert(object);
     },
     update: function(id, changes, collectionName) {
         var collection;
-        var schema;
+
 
         if (! W2UI_METEOR.PermissionsHelper.isAllowMethodUpdate()) return;
 
-        collection = Mongo.Collection.get(collectionName);
-        schema = collection.simpleSchema();
         check(id, String);
-        _.each(_.keys(changes), function(key) {
-            var specimen = {};
-            specimen[key] = changes[key];
-            check(specimen, schema.pick(key)); // https://github.com/aldeed/meteor-simple-schema/issues/251
-        });
+        check(collectionName, String);
 
+        collection = Mongo.Collection.get(collectionName);
         return collection.update(id, {$set: changes});
     },
     remove: function(ids, collectionName) {
@@ -35,8 +26,10 @@ Meteor.methods({
 
         if (! W2UI_METEOR.PermissionsHelper.isAllowMethodRemove()) return;
 
-        collection = Mongo.Collection.get(collectionName);
         check(ids, [String]);
+        check(collectionName, String);
+
+        collection = Mongo.Collection.get(collectionName);
         _.each(ids, function(id) {collection.remove(id)});
     }
 });
